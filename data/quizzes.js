81498f85733f416569a0942734cbcb50c44258a3
classenpoche <stegerp@wanadoo.fr>
2026-07-04 15:06:43 +0200

    Add math quizzes to quizzes.js

--- data/quizzes.js
+++ data/quizzes.js
@@ -1,1 +1,15 @@
 
+export const quizzes = {
+  "math_6e_add": [
+    {
+      q: "2 + 2 = ?",
+      choices: ["3", "4", "5"],
+      answer: 1
+    },
+    {
+      q: "5 + 3 = ?",
+      choices: ["6", "7", "8"],
+      answer: 2
+    }
+  ]
+};
