087abfecb61a8407ec59562623fce0916287d09f
classenpoche <stegerp@wanadoo.fr>
2026-07-04 22:07:03 +0200

    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,52 @@
 
+const CACHE_NAME = "classenpoche-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./style.css",
+  "./subjects.js",
+  "./levels.js",
+  "./courses.js",
+  "./quizzes.js"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// ACTIVATE (SUPPRIME ANCIENS CACHES)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH (STRATEGIE PROPRE)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
