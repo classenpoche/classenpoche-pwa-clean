f35e069578c9f5dcb09ed54fd4c5baa7c3d4ad49
classenpoche <stegerp@wanadoo.fr>
2026-07-04 22:12:43 +0200

    Simplify cache deletion logic in service worker

--- service-worker.js
+++ service-worker.js
@@ -27,14 +27,11 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
+        keys.map((key) => caches.delete(key))
       );
     })
   );
+
   self.clients.claim();
 });
 
