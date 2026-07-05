5cafb1eb12682cceeb09dcfaf3f0f099674a18cd
classenpoche <stegerp@wanadoo.fr>
2026-07-05 11:51:45 +0200

    Update subjects list with new entries and emojis
    Added new subjects with emojis and updated existing names.

--- data/subjects.js
+++ data/subjects.js
@@ -1,5 +1,26 @@
 export const subjects = [
-  { id: "math", name: "Mathématiques" },
-  { id: "fr", name: "Français" },
-  { id: "ang", name: "Anglais" }
+  {
+    id: "math",
+    name: "➗ Mathématiques"
+  },
+  {
+    id: "fr",
+    name: "📖 Français"
+  },
+  {
+    id: "ang",
+    name: "🇬🇧 Anglais"
+  },
+  {
+    id: "hist",
+    name: "🏛 Histoire"
+  },
+  {
+    id: "geo",
+    name: "🌍 Géographie"
+  },
+  {
+    id: "svt",
+    name: "🧬 SVT"
+  }
 ];
