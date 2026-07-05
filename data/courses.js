b185603672e67d55da542f15e3b8633a84d089eb
classenpoche <stegerp@wanadoo.fr>
2026-07-05 11:53:35 +0200

    Add new courses to courses.js

--- data/courses.js
+++ data/courses.js
@@ -1,8 +1,62 @@
 export const courses = [
+
+  // MATHS
   {
     id: "math_6e_add",
     subject: "math",
     level: "6e",
     title: "Les additions"
+  },
+  {
+    id: "math_6e_frac",
+    subject: "math",
+    level: "6e",
+    title: "Les fractions"
+  },
+
+  // FRANÇAIS
+  {
+    id: "fr_6e_gram",
+    subject: "fr",
+    level: "6e",
+    title: "La grammaire"
+  },
+  {
+    id: "fr_5e_conj",
+    subject: "fr",
+    level: "5e",
+    title: "La conjugaison"
+  },
+
+  // ANGLAIS
+  {
+    id: "ang_6e_vocab",
+    subject: "ang",
+    level: "6e",
+    title: "Le vocabulaire"
+  },
+
+  // HISTOIRE
+  {
+    id: "hist_6e_egypte",
+    subject: "hist",
+    level: "6e",
+    title: "L'Égypte antique"
+  },
+
+  // GÉOGRAPHIE
+  {
+    id: "geo_6e_planete",
+    subject: "geo",
+    level: "6e",
+    title: "La planète Terre"
+  },
+
+  // SVT
+  {
+    id: "svt_6e_cellule",
+    subject: "svt",
+    level: "6e",
+    title: "La cellule"
   }
 ];
