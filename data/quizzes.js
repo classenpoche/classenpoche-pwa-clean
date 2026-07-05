9297cd7349c6683934de6634a7bbccc49dd7b02c
classenpoche <stegerp@wanadoo.fr>
2026-07-05 11:54:48 +0200

    Add multiple quizzes for various subjects

--- data/quizzes.js
+++ data/quizzes.js
@@ -1,5 +1,7 @@
 export const quizzes = {
-  "math_6e_add": [
+
+  // MATHS
+  math_6e_add: [
     {
       q: "2 + 2 = ?",
       choices: ["3", "4", "5"],
@@ -9,6 +11,87 @@ export const quizzes = {
       q: "5 + 3 = ?",
       choices: ["6", "7", "8"],
       answer: 2
+    }
+  ],
+
+  math_6e_frac: [
+    {
+      q: "1/2 + 1/2 = ?",
+      choices: ["1", "2", "3"],
+      answer: 0
+    }
+  ],
+
+  // FRANÇAIS
+  fr_6e_gram: [
+    {
+      q: "Le verbe dans 'Je mange' est :",
+      choices: ["Je", "mange", "dans"],
+      answer: 1
+    }
+  ],
+
+  fr_5e_conj: [
+    {
+      q: "Passé composé de 'venir' :",
+      choices: [
+        "je viens",
+        "je suis venu",
+        "je venais"
+      ],
+      answer: 1
+    }
+  ],
+
+  // ANGLAIS
+  ang_6e_vocab: [
+    {
+      q: "'Dog' signifie :",
+      choices: [
+        "chat",
+        "chien",
+        "oiseau"
+      ],
+      answer: 1
+    }
+  ],
+
+  // HISTOIRE
+  hist_6e_egypte: [
+    {
+      q: "Le Nil est un :",
+      choices: [
+        "fleuve",
+        "lac",
+        "océan"
+      ],
+      answer: 0
+    }
+  ],
+
+  // GÉOGRAPHIE
+  geo_6e_planete: [
+    {
+      q: "La Terre est :",
+      choices: [
+        "plate",
+        "une planète",
+        "une étoile"
+      ],
+      answer: 1
+    }
+  ],
+
+  // SVT
+  svt_6e_cellule: [
+    {
+      q: "L'unité du vivant est :",
+      choices: [
+        "l'atome",
+        "la cellule",
+        "la molécule"
+      ],
+      answer: 1
     }
   ]
 };
