6a50a6c38ac871745629258800912fb4f533cb3d
classenpoche <stegerp@wanadoo.fr>
2026-07-22 17:17:09 +0200

    Add new levels to levels.js

--- data/levels.js
+++ data/levels.js
@@ -15,5 +15,21 @@ export const levels = [
   {
     id: "3e",
     name: "3ème"
+  },
+  {
+    id: "2de",
+    name: "2de"
+  },
+{
+    id: "1e",
+    name: "1ère"
+  },
+  {
+    id: "Term",
+    name: "Term"
+  },
+  {
+    id: "Bts",
+    name: "Bts"
   }
 ];
