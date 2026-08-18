7522e2c746e25ff4eece8932e91b0210be84f79b
classenpoche <stegerp@wanadoo.fr>
2026-08-18 21:48:46 +0200

    Add courses data and update HTML structure

--- app-v2.js
+++ app-v2.js
@@ -2,18 +2,21 @@ alert("NOUVEL APP-V2");
 
 import { subjects } from "./data/subjects.js";
 import { levels } from "./data/levels.js";
+import { courses } from "./data/courses.js";
 
 console.log("SUBJECTS :", subjects);
 console.log("LEVELS :", levels);
+console.log("COURSES :", courses);
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const app = document.getElementById("app");
 
     app.innerHTML = `
-        <h1>TEST LEVELS</h1>
+        <h1>TEST COURSES</h1>
         <p>Matières : ${subjects.length}</p>
         <p>Niveaux : ${levels.length}</p>
+        <p>Cours : ${courses.length}</p>
     `;
 
 });
