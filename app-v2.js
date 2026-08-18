3f14ed4f39653fbab119b731da401eebefa8bd86
classenpoche <stegerp@wanadoo.fr>
2026-08-18 21:50:37 +0200

    Integrate quizzes and update app content
    Added quizzes data and updated DOM content to include quiz information.

--- app-v2.js
+++ app-v2.js
@@ -3,20 +3,23 @@ alert("NOUVEL APP-V2");
 import { subjects } from "./data/subjects.js";
 import { levels } from "./data/levels.js";
 import { courses } from "./data/courses.js";
+import { quizzes } from "./data/quizzes.js";
 
 console.log("SUBJECTS :", subjects);
 console.log("LEVELS :", levels);
 console.log("COURSES :", courses);
+console.log("QUIZZES :", quizzes);
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const app = document.getElementById("app");
 
     app.innerHTML = `
-        <h1>TEST COURSES</h1>
+        <h1>TEST QUIZZES</h1>
         <p>Matières : ${subjects.length}</p>
         <p>Niveaux : ${levels.length}</p>
         <p>Cours : ${courses.length}</p>
+        <p>Quiz : ${quizzes.length}</p>
     `;
 
 });
