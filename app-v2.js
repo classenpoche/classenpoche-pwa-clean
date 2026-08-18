df60e415287c9fe45c080975c3b36824ab2552f3
classenpoche <stegerp@wanadoo.fr>
2026-08-18 21:46:45 +0200

    Add levels import and update DOM content

--- app-v2.js
+++ app-v2.js
@@ -1,16 +1,19 @@
 alert("NOUVEL APP-V2");
 
 import { subjects } from "./data/subjects.js";
+import { levels } from "./data/levels.js";
 
 console.log("SUBJECTS :", subjects);
+console.log("LEVELS :", levels);
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const app = document.getElementById("app");
 
     app.innerHTML = `
-        <h1>TEST SUBJECTS</h1>
-        <p>Nombre de matières : ${subjects.length}</p>
+        <h1>TEST LEVELS</h1>
+        <p>Matières : ${subjects.length}</p>
+        <p>Niveaux : ${levels.length}</p>
     `;
 
 });
